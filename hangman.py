4cfea5dec96f4928adf367856f91f871ee472891
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2017-10-18 15:25:01 +0200

    Death and tries systems implemented

--- hangman.py
+++ hangman.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-#TODO Users and Tries + Death system(maximum tries)
+#TODO Users
 
 import random, requests
 
-lenght = int(input("De cuantas letras quieres la palabra: "))
-adivinando = []
+lenght = int(input("How many characters should the word have: "))
+guessing = []
+tries = 0
 
 for i in range(lenght):
-    adivinando.append("_") #generating empty spaces
+    guessing.append("_") #generating empty spaces
 
 def randomword(lenght):
     word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
@@ -23,16 +24,21 @@ def randomword(lenght):
 word = randomword(lenght)
 
 while True:
-    matches = [m for m in adivinando if m == "_"]
-    if not matches: #check if list is empty
-        print("Congraz you won")
-        break
-    letra = input("Dime una letra: ")
-    if letra in word:
-        print("Muy bien la letra", letra, "esta en la palabra")
-        indices = [pos for pos, char in enumerate(word) if char == letra]
-        for posicion in indices:
-            adivinando[posicion] = letra
+    if tries != 20: #death system
+        matches = [m for m in guessing if m == "_"]
+        if not matches: #check if list is empty
+            print("Congraz you won with", tries, "tries")
+            break
+        character = input("Gimme a character: ")
+        if character in word:
+            print("Alright! the character", character, "is in the word")
+            indices = [pos for pos, char in enumerate(word) if char == character]
+            for posicion in indices:
+                guessing[posicion] = character
+        else:
+            print("Damn..!", character, "is not in the word")
+        tries += 1
+        print(' '.join(guessing))
     else:
-        print("La letra", letra, "no esta en la palabra")
-    print(' '.join(adivinando))
+        print("You exceeded the 20 tries, you lose..")
+        break
