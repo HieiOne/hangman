15c38742471a624097e9686605052832311dff37
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2017-10-18 18:13:49 +0200

    New death system, users implemented

--- hangman.py
+++ hangman.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
-#TODO Users
+#TODO If user got a character right, grant it a free-turn and explain the game
 
 import random, requests
-
-lenght = int(input("How many characters should the word have: "))
-guessing = []
-tries = 0
-
-for i in range(lenght):
-    guessing.append("_") #generating empty spaces
 
 def randomword(lenght):
     word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
@@ -21,24 +14,50 @@ def randomword(lenght):
             accepted_words.append(every_word)
     word = random.choice(accepted_words)
     return word
-word = randomword(lenght)
 
-while True:
-    if tries != 20: #death system
-        matches = [m for m in guessing if m == "_"]
-        if not matches: #check if list is empty
-            print("Congraz you won with", tries, "tries")
-            break
-        character = input("Gimme a character: ")
-        if character in word:
-            print("Alright! the character", character, "is in the word")
-            indices = [pos for pos, char in enumerate(word) if char == character]
-            for posicion in indices:
-                guessing[posicion] = character
-        else:
-            print("Damn..!", character, "is not in the word")
-        tries += 1
-        print(' '.join(guessing))
+def game(tries, name):
+    print("It's user", name, "turn")
+    print(list_users[user][1])
+    matches = [m for m in guessing if m == "_"]
+    if not matches: #check if list is empty
+        print("Congraz you won with", tries, "tries")
+
+    character = input("Gimme a character: ")
+    if character in word:
+        print("Alright! the character", character, "is in the word")
+        indices = [pos for pos, char in enumerate(word) if char == character]
+        for posicion in indices:
+            guessing[posicion] = character
     else:
-        print("You exceeded the 20 tries, you lose..")
-        break
+        print("Damn..!", character, "is not in the word")
+        
+    list_users[user][1] += 1 #+1 to tries of the user which tried
+    print(' '.join(guessing))
+
+#everything starts here
+qusers = int(input("How many users: "))
+lenght = int(input("How many characters should the word have: "))
+guessing = [] 
+list_users = [] #name = [0], tries = [1]
+word = randomword(lenght)
+print(word)
+
+for user in range(qusers):
+    list = []
+    name = input("Your name: ")
+    tries = 0
+    list.append(name)
+    list.append(tries)
+    list_users.append(list)
+
+for i in range(lenght):
+    guessing.append("_") #generating empty spaces
+
+for _ in range(21+qusers): #to give turns to the other users
+    for user in range(len(list_users)): #we use range to iterate between lists
+        if list_users[user][1] <= 20:
+            print(list_users[user][1])
+            game(list_users[user][1], list_users[user][0])
+        else:
+            #add message to inform about the lost
+            pass #not effective , player 1 will lose first, and leave player 2 with 19 turns
