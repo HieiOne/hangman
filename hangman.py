80dc2f99c382e8b8d668ed9bcbbcc9bfd46f1a37
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2017-10-18 14:49:33 +0200

    Now uses a online dictionary and added win system

--- hangman.py
+++ hangman.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
-import random, string, requests
+#TODO Users and Tries + Death system(maximum tries)
+
+import random, requests
 
 lenght = int(input("De cuantas letras quieres la palabra: "))
-
-def randomword(lenght):
-    word = ''.join(random.choice(string.ascii_lowercase) for i in range(lenght))
-    return word
-
-word = randomword(lenght)
 adivinando = []
 
 for i in range(lenght):
-    adivinando.append("_")
+    adivinando.append("_") #generating empty spaces
+
+def randomword(lenght):
+    word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+    dictionary = requests.get(word_site)
+    #.text formats the words, and .splitlines() let's me grab a word
+    words = dictionary.text.splitlines()
+    accepted_words = []
+    for every_word in words:
+        if len(every_word) == lenght:
+            accepted_words.append(every_word)
+    word = random.choice(accepted_words)
+    return word
+word = randomword(lenght)
 
 while True:
-    #print(word)
+    matches = [m for m in adivinando if m == "_"]
+    if not matches: #check if list is empty
+        print("Congraz you won")
+        break
     letra = input("Dime una letra: ")
     if letra in word:
         print("Muy bien la letra", letra, "esta en la palabra")
