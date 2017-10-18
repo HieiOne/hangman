85bf6ad3d5d6ab054254128e72a415475b04f1e1
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2017-10-18 21:52:31 +0200

    show an abecedary with the characters that hasn't been typen yet, stop being case sensitive, fixed bug show an abecedary with the characters that hasn't been typen yet, stop being case sensitive, fixed bug

--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-#TODO show an abecedary with the characters that hasn't been typen yet | stop being case sensitive
 
-import random, requests
+import random, requests, string
 
 class HangmanDone(Exception): pass #to be able to break the loop
 
@@ -20,22 +19,27 @@ def randomword(lenght):
 def game(tries, name):
     print("It's user", name, "turn")
     matches = [m for m in guessing if m == "_"]
+
     if not matches: #check if list is empty
-        print("Congraz you won with", tries, "tries")
+        print("Congraz", name, " won with", tries, "tries")
         raise HangmanDone
+
+    print(' | '.join(letters)) #print letters that haven't been used yet
     character = input("Gimme a character: ")
 
     if character in guessing: #if character was already typen, make it to be incorrect if typen again
         print("This letter was already typen, I won't count this try but is not your turn anymore")
-    elif character in word:
-        print("Alright! the character", character, "is in the word")
-        indices = [pos for pos, char in enumerate(word) if char == character]
+    elif character.upper() in word.upper(): #stop being case sensitive
+        print("Alright! the character", character.lower(), "is in the word")
+        letters.remove(character.upper())
+        indices = [pos for pos, char in enumerate(word) if char == character.lower()]
         for posicion in indices:
-            guessing[posicion] = character
-            print(' '.join(guessing))
-            game(tries, name) #gives back the turn without suming any try
+            guessing[posicion] = character.lower()
+        print(' '.join(guessing))
+        game(tries, name) #gives back the turn without suming any try
     else:
         print("Damn..!", character, "is not in the word")
+        letters.remove(character.upper())
         print(' '.join(guessing))
         list_users[user][1] += 1 #+1 to tries of the user which tried  
 
@@ -44,8 +48,9 @@ qusers = int(input("How many users: "))
 lenght = int(input("How many characters should the word have: "))
 guessing = [] 
 list_users = [] #name = [x][0], tries = [x][1]
+letters = list(string.ascii_uppercase) # this is a dictionary even if it doesn't look like one
 word = randomword(lenght)
-print(word)
+#print(word) #cheats on
 
 for user in range(qusers):
     list = []
